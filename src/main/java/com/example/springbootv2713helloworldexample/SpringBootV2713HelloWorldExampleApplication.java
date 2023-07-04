7b462b5a9b958601cfff8aac34871e8996681c1c
mirpribili <mirpribili@ya.ru>
2023-07-04 18:25:28 +0300

    how create a custom properties

--- src/main/java/com/example/springbootv2713helloworldexample/SpringBootV2713HelloWorldExampleApplication.java
+++ src/main/java/com/example/springbootv2713helloworldexample/SpringBootV2713HelloWorldExampleApplication.java
@@ -1,5 +1,7 @@
 package com.example.springbootv2713helloworldexample;
 
+import com.example.springbootv2713helloworldexample.config.ConfigProperties;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.CommandLineRunner;
@@ -10,15 +12,19 @@ import java.lang.reflect.Array;
 import java.util.Arrays;
 
 @SpringBootApplication
-public class SpringBootV2713HelloWorldExampleApplication implements ApplicationRunner {
+public class SpringBootV2713HelloWorldExampleApplication implements CommandLineRunner {
+	@Autowired
+	private ConfigProperties configProperties;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootV2713HelloWorldExampleApplication.class, args);
 	}
 
 	@Override
-	public void run(ApplicationArguments args) throws Exception {
-		System.out.println(" *** Show arguments *** ");
-		Arrays.stream(args.getSourceArgs()).forEach(System.out::println);
+	public void run(String... args) throws Exception {
+		System.out.println(" *** mail properties *** ");
+		System.out.println("from:"+configProperties.getFrom());
+		System.out.println("to:"+configProperties.getTo());
+		System.out.println("default object:"+configProperties.getDefaultObject());
 	}
 }
