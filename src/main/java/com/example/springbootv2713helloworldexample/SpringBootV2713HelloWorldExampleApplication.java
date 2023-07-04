abee79f74c654a8c146e4a554e14e92b12af3eec
mirpribili <mirpribili@ya.ru>
2023-07-04 16:32:29 +0300

    test use ApplicationRunner

--- src/main/java/com/example/springbootv2713helloworldexample/SpringBootV2713HelloWorldExampleApplication.java
+++ src/main/java/com/example/springbootv2713helloworldexample/SpringBootV2713HelloWorldExampleApplication.java
@@ -1,5 +1,7 @@
 package com.example.springbootv2713helloworldexample;
 
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,26 +10,15 @@ import java.lang.reflect.Array;
 import java.util.Arrays;
 
 @SpringBootApplication
-public class SpringBootV2713HelloWorldExampleApplication implements CommandLineRunner {
+public class SpringBootV2713HelloWorldExampleApplication implements ApplicationRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootV2713HelloWorldExampleApplication.class, args);
 	}
 
 	@Override
-	public void run(String... args) throws Exception {
+	public void run(ApplicationArguments args) throws Exception {
 		System.out.println(" *** Show arguments *** ");
-		Arrays.stream(args).forEach(System.out::println);
+		Arrays.stream(args.getSourceArgs()).forEach(System.out::println);
 	}
-/*
-if I want war file
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder){
-		return super.configure(builder);
-	}
-
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootV2713HelloWorldExampleApplication.class, args);
-	}
-* */
 }
