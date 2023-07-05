c976642207f433e828118aa0b458a51a1ea20bc5
mirpribili <mirpribili@ya.ru>
2023-07-05 14:53:56 +0300

    default

--- src/main/java/com/example/springbootv2713helloworldexample/SpringBootV2713HelloWorldExampleApplication.java
+++ src/main/java/com/example/springbootv2713helloworldexample/SpringBootV2713HelloWorldExampleApplication.java
@@ -1,37 +1,14 @@
 package com.example.springbootv2713helloworldexample;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.lang.reflect.Array;
-import java.util.Arrays;
 
 @SpringBootApplication
-public class SpringBootV2713HelloWorldExampleApplication implements CommandLineRunner {
+public class SpringBootV2713HelloWorldExampleApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootV2713HelloWorldExampleApplication.class, args);
 	}
 
-	@Override
-	public void run(String... args) throws Exception {
-		System.out.println(" *** Show arguments *** ");
-		Arrays.stream(args).forEach(System.out::println);
-		// for old java
-		for (int i=0;i< args.length; i++){
-			System.out.println(args[i]);
-		}
-	}
-/*
-if I want war file
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder){
-		return super.configure(builder);
-	}
-
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootV2713HelloWorldExampleApplication.class, args);
-	}
-* */
 }
