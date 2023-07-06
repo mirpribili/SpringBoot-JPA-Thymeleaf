b4b436b7703c81b08a6a00a4490b6a28f622f430
mirpribili <mirpribili@ya.ru>
2023-07-06 15:58:33 +0300

    create new Database Postgres and add connection properties

--- src/main/java/com/example/librarymanagementsystem/LibraryManagementSystemApplication.java
+++ src/main/java/com/example/librarymanagementsystem/LibraryManagementSystemApplication.java
@@ -12,3 +12,19 @@ public class LibraryManagementSystemApplication {
 	// Create Entity and Repository
 
 }
+/**
+ * install postgreSQL & pgadmin4:
+ * sudo apt update
+ * sudo apt install postgresql -y
+ * sudo -i -u postgres
+ * psql
+ * ALTER USER postgres PASSWORD 'pass';
+ * \q
+ * exit
+ *
+ * curl -fsS https://www.pgadmin.org/static/packages_pgadmin_org.pub | sudo gpg --dearmor -o /usr/share/keyrings/packages-pgadmin-org.gpg
+ * sudo sh -c 'echo "deb [signed-by=/usr/share/keyrings/packages-pgadmin-org.gpg] https://ftp.postgresql.org/pub/pgadmin/pgadmin4/apt/$(lsb_release -cs) pgadmin4 main" > /etc/apt/sources.list.d/pgadmin4.list && apt update'
+ * sudo apt install pgadmin4 -y
+ *
+ * GUI pgadmin4 -- Databases add DB: libraryDB
+ */
