b28458ef100df519a878ba2c157c75cead186929
mirpribili <mirpribili@ya.ru>
2023-07-06 16:47:53 +0300

    Deploy and Test using Postman

--- src/main/java/com/example/librarymanagementsystem/LibraryManagementSystemApplication.java
+++ src/main/java/com/example/librarymanagementsystem/LibraryManagementSystemApplication.java
@@ -1,16 +1,39 @@
 package com.example.librarymanagementsystem;
 
+import com.example.librarymanagementsystem.entity.Book;
+import com.example.librarymanagementsystem.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+import java.util.List;
+
 @SpringBootApplication
-public class LibraryManagementSystemApplication {
+public class LibraryManagementSystemApplication implements ApplicationRunner {
+
+	@Autowired
+	private BookService bookService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(LibraryManagementSystemApplication.class, args);
 	}
-	// Create Entity and Repository
 
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+
+		List<Book> books = bookService.findAllBooks();
+		if(books != null && books.isEmpty()) {
+			Book book1 = Book.builder().name("Book 1").description("description of book 1").price(12.5).build();
+			Book book2 = Book.builder().name("Book 2").description("description of book 2").price(15).build();
+			Book book3 = Book.builder().name("Book 3").description("description of book 3").price(20).build();
+
+			Arrays.asList(book1, book2, book3).forEach(b -> bookService.createBook(b));
+			System.out.println("New books added in database");
+		}
+	}
 }
 /**
  * install postgreSQL & pgadmin4:
@@ -27,4 +50,7 @@ public class LibraryManagementSystemApplication {
  * sudo apt install pgadmin4 -y
  *
  * GUI pgadmin4 -- Databases add DB: libraryDB
+ *
+ * install postman
+ * sudo snap install postman
  */
