ab5295b5c20f4ea673cd4d41cef182c992a07b98
mirpribili <mirpribili@ya.ru>
2023-07-07 12:21:39 +0300

    Securing the Library Management System using Spring Boot and Spring Security

---
+++ src/main/java/com/example/librarymanagementsystem/config/SecurityConfiguration.java
@@ -0,0 +1,26 @@
+package com.example.librarymanagementsystem.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/edit/**").hasAnyRole(UserRole.ADMIN.name(),UserRole.PUBLISHER.name())
+                .antMatchers("/delete/**").hasAnyRole(UserRole.ADMIN.name())
+                .anyRequest().authenticated()
+                .and()
+                .formLogin();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user_admin").password("{noop}1234").roles(UserRole.ADMIN.name())
+                .and().withUser("user_publisher").password("{noop}123").roles(UserRole.PUBLISHER.name())
+                .and().withUser("user_read_only").password("{noop}12").roles(UserRole.READ_ONLY.name());
+    }
+}
