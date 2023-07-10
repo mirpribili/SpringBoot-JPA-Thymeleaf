f5ec69e258c2ee1a92c904c2b2268af6ddb01a20
mirpribili <mirpribili@ya.ru>
2023-07-10 17:35:24 +0300

    create 'Instructor' ORM Annotations

--- src/main/java/com/javacorner/admin/entity/Instructor.java
+++ src/main/java/com/javacorner/admin/entity/Instructor.java
@@ -1,17 +1,39 @@
 package com.javacorner.admin.entity;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
-
+@Entity
+@Table(name = "instructors")
 public class Instructor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "instructor_id", nullable = false)
     private Long instructorId;
+    @Basic
+    @Column(name = "first_name", nullable = false, length = 45)
     private String firstName;
+    @Basic
+    @Column(name = "last_name", nullable = false, length = 45)
     private String lastName;
+    @Basic
+    @Column(name = "summary", nullable = false, length = 64)
     private String summary;
 
+    /**
+     * com.javacorner.admin.entity.Course.java
+     * ...
+     * @ManyToOne(fetch = FetchType.LAZY)
+     * @JoinColumn(name = "instructor_id", referencedColumnName = "instructor_id", nullable = false)
+     * private Instructor instructor;
+     * ...
+     * */
+    @OneToMany(mappedBy = "instructor"/* ! */, fetch = FetchType.LAZY) // see up ^^
     private Set<Course> courseSet = new HashSet<>();
 
+    @OneToOne(cascade = CascadeType.REMOVE)
+    @JoinColumn(name = "user_id", referencedColumnName = "user_id", nullable = false)
     private User user;
 
     @Override
