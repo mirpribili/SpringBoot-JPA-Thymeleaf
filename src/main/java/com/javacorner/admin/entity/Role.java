355d760f1ce6c6baa80e371cab8787a2a12d9b44
mirpribili <mirpribili@ya.ru>
2023-07-10 18:01:21 +0300

    create 'Role' ORM Annotations

--- src/main/java/com/javacorner/admin/entity/Role.java
+++ src/main/java/com/javacorner/admin/entity/Role.java
@@ -1,13 +1,29 @@
 package com.javacorner.admin.entity;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
-
+@Entity
+@Table(name = "roles")
 public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "role_id", nullable = false)
     private Long roleId;
+    @Basic
+    @Column(name = "name", nullable = false, length = 45, unique = true)
     private String name;
-
+    /**
+     * com.javacorner.admin.entity.User
+     *
+     *     @ManyToMany(fetch = FetchType.EAGER) //LAZY = fetch when needed, EAGER = fetch immediately!!
+     *     @JoinTable(name = "user_role",
+     *         joinColumns = {@JoinColumn(name = "user_id")},
+     *         inverseJoinColumns = {@JoinColumn(name = "role_id")})
+     *     private Set<Role> roles = new HashSet<>();
+     */
+    @ManyToMany(mappedBy = "roles", fetch = FetchType.LAZY)
     private Set<User> users = new HashSet<>();
 
     public Long getRoleId() {
