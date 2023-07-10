53d8e885d685cac9c1a1e000f66ef7c15fc3aa8e
mirpribili <mirpribili@ya.ru>
2023-07-10 15:51:10 +0300

    create Entities & Relationships

--- src/main/java/com/javacorner/admin/entity/Instructor.java
+++ src/main/java/com/javacorner/admin/entity/Instructor.java
@@ -1,12 +1,18 @@
 package com.javacorner.admin.entity;
 
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 public class Instructor {
     private Long instructorId;
     private String firstName;
     private String lastName;
     private String summary;
+
+    private Set<Course> courseSet = new HashSet<>();
+
+    private User user;
 
     @Override
     public boolean equals(Object o) {
@@ -19,5 +25,73 @@ public class Instructor {
     @Override
     public int hashCode() {
         return Objects.hash(instructorId, firstName, lastName, summary);
+    }
+
+    public Instructor() {
+    }
+
+    @Override
+    public String toString() {
+        return "Instructor{" +
+                "instructorId=" + instructorId +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", summary='" + summary + '\'' +
+                '}';
+    }
+
+    public Instructor(String firstName, String lastName, String summary, User user) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.summary = summary;
+        this.user = user;
+    }
+
+    public Long getInstructorId() {
+        return instructorId;
+    }
+
+    public void setInstructorId(Long instructorId) {
+        this.instructorId = instructorId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getSummary() {
+        return summary;
+    }
+
+    public void setSummary(String summary) {
+        this.summary = summary;
+    }
+
+    public Set<Course> getCourseSet() {
+        return courseSet;
+    }
+
+    public void setCourseSet(Set<Course> courseSet) {
+        this.courseSet = courseSet;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
     }
 }
--- src/main/java/com/javacorner/admin/entity/Student.java
+++ src/main/java/com/javacorner/admin/entity/Student.java
@@ -1,6 +1,8 @@
 package com.javacorner.admin.entity;
 
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 public class Student {
     private Long studentId;
@@ -8,13 +10,37 @@ public class Student {
     private String lastName;
     private String level;
 
+    private Set<Course> courses = new HashSet<>();
+
+    private User user;
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Student student = (Student) o;
-        // studentId can nat be null
+        // studentId can not be null
         return studentId.equals(student.studentId) && Objects.equals(firstName, student.firstName) && Objects.equals(lastName, student.lastName) && Objects.equals(level, student.level);
+    }
+
+    public Student() {
+    }
+
+    public Student(String firstName, String lastName, String level, User user) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.level = level;
+        this.user = user;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", level='" + level + '\'' +
+                ", user=" + user +
+                '}';
     }
 
     @Override
