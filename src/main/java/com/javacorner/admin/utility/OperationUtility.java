6fe803cc92c53d7c27f37aaf5f50114d12c866da
mirpribili <mirpribili@ya.ru>
2023-07-11 18:21:28 +0300

    Developing Helper Class to Test the Functionalities 'Role' Operations

--- src/main/java/com/javacorner/admin/utility/OperationUtility.java
+++ src/main/java/com/javacorner/admin/utility/OperationUtility.java
@@ -1,9 +1,12 @@
 package com.javacorner.admin.utility;
 
+import com.javacorner.admin.dao.RoleDao;
 import com.javacorner.admin.dao.UserDao;
+import com.javacorner.admin.entity.Role;
 import com.javacorner.admin.entity.User;
 
 import javax.persistence.EntityNotFoundException;
+import java.util.List;
 
 
 public class OperationUtility {
@@ -12,6 +15,47 @@ public class OperationUtility {
         updateUser(userDao);
         deleteUser(userDao);
         fetchUsers(userDao);
+    }
+
+    public static void roleOperations(RoleDao roleDao){
+        createRoles(roleDao);
+        updateRole(roleDao);
+        deleteRole(roleDao);
+        fetchRole(roleDao);
+    }
+
+    public static void assignRolesToUsers(UserDao userDao, RoleDao roleDao){
+        Role role = roleDao.findByName("Admin");
+        if(role==null) throw new EntityNotFoundException("Role Not Found");
+        List<User> users = userDao.findAll();
+        users.forEach(user ->{
+            user.assignRoleToUser(role);
+            userDao.save(user);
+                });
+    }
+
+    private static void fetchRole(RoleDao roleDao) {
+        roleDao.findAll().forEach(role -> System.out.println(role.toString()));
+    }
+
+    private static void deleteRole(RoleDao roleDao) {
+        //not need:  Role role = roleDao.findById(2L).orElseThrow(()-> new EntityNotFoundException("Role Not Found"));
+        // see ctrl+alt+B
+        roleDao.deleteById(2L);
+    }
+
+    public static void updateRole(RoleDao roleDao){
+        Role role = roleDao.findById(1L).orElseThrow(()-> new EntityNotFoundException("Role Not Found"));
+        role.setName("NewAdmin");
+        roleDao.save(role);
+    }
+    public static void createRoles(RoleDao roleDao){
+        Role role1 = new Role("Admin");
+        roleDao.save(role1);
+        Role role2 = new Role("Instructor");
+        roleDao.save(role2);
+        Role role3 = new Role("Student");
+        roleDao.save(role3);
     }
     public static void createUsers(UserDao userDao){
         User user1 = new User("user1@gmail.com", "pass1");
