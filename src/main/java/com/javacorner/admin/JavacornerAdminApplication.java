bcdfa9a066d9b93dce6c6dde1f37cd2583e155fe
mirpribili <mirpribili@ya.ru>
2023-07-12 17:45:07 +0300

    Remove Utility Package

--- src/main/java/com/javacorner/admin/JavacornerAdminApplication.java
+++ src/main/java/com/javacorner/admin/JavacornerAdminApplication.java
@@ -1,7 +1,6 @@
 package com.javacorner.admin;
 
 import com.javacorner.admin.dao.*;
-import com.javacorner.admin.utility.OperationUtility;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -36,13 +35,6 @@ public class JavacornerAdminApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		//OperationUtility.userOperations(userDao);
-		//OperationUtility.roleOperations(roleDao);
-		//OperationUtility.assignRolesToUsers(userDao,roleDao);
-		//OperationUtility.instructorsOperations(userDao, instructorDao, roleDao);
-		//OperationUtility.studentOperations(userDao, studentDao, roleDao);
 
-		//OperationUtility.createRolesUsersInstructors(userDao,roleDao,instructorDao,studentDao);
-		//OperationUtility.courseOperations(courseDao, instructorDao, studentDao);
 	}
 }
