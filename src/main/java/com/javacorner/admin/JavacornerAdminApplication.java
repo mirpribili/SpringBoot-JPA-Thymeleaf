92cdd3ef74efd9ee1e6bab3bf5abe175f75f3cd3
mirpribili <mirpribili@ya.ru>
2023-07-12 15:18:14 +0300

    Test User Operations

--- src/main/java/com/javacorner/admin/JavacornerAdminApplication.java
+++ src/main/java/com/javacorner/admin/JavacornerAdminApplication.java
@@ -1,13 +1,32 @@
 package com.javacorner.admin;
 
+import com.javacorner.admin.dao.*;
+import com.javacorner.admin.utility.OperationUtility;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JavacornerAdminApplication {
+public class JavacornerAdminApplication implements CommandLineRunner {
+	@Autowired
+	private UserDao userDao;
+	@Autowired
+	private CourseDao courseDao;
+	@Autowired
+	private InstructorDao instructorDao;
+	@Autowired
+	private StudentDao studentDao;
+	@Autowired
+	private RoleDao roleDao;
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(JavacornerAdminApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		OperationUtility.userOperations(userDao);
+	}
 }
