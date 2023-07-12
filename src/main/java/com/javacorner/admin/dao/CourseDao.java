83733eb0b2d42453a44e7908214168b0a2b80078
mirpribili <mirpribili@ya.ru>
2023-07-12 17:23:52 +0300

    fix and test crud entity.Course

--- src/main/java/com/javacorner/admin/dao/CourseDao.java
+++ src/main/java/com/javacorner/admin/dao/CourseDao.java
@@ -1,6 +1,7 @@
 package com.javacorner.admin.dao;
 
 import com.javacorner.admin.entity.Course;
+import com.javacorner.admin.entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -10,8 +11,7 @@ import java.util.List;
 public interface CourseDao extends JpaRepository<Course, Long> {
     List<Course> findCoursesByCourseNameContains(String keyword); // create with ultimate ver hints
 
-    @Query(value =
-            "select * from courses as c where c.course_id (select e.course_id from enrolled_in as e where e.student_id=:studentId)",
+    @Query(value = "select * from courses as c where c.course_id in (select e.course_id from enrolled_in as e where e.student_id=:studentId)",
             nativeQuery = true)
     List<Course> getCoursesByStudentId(@Param("studentId") Long studentId);
 }
