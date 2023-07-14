d2d9d259c29f3de7846270f9465bb992d284d015
mirpribili <mirpribili@ya.ru>
2023-07-14 16:17:29 +0300

    Configuring Test Containers. Create MySQL Container

---
+++ src/test/java/com/javacorner/admin/AbstractTest.java
@@ -0,0 +1,21 @@
+package com.javacorner.admin;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MySQLContainer;
+
+public class AbstractTest {
+    private static MySQLContainer container = new MySQLContainer<>("mysql:latest");
+    @DynamicPropertySource
+    public static void overrideProps(DynamicPropertyRegistry registry){
+        registry.add("spring.datasource.url",container::getJdbcUrl);
+        registry.add("spring.datasource.username",container::getUsername);
+        registry.add("spring.datasource.password",container::getPassword);
+    }
+    @BeforeAll
+    public static void  setUp(){
+        container.start();
+        // with that, we have added all the configurations that enables us to create a MySQL container for our testing
+    }
+}
