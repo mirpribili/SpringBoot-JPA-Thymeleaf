2e5d350a81f6600f655e53a10bf949dceaeffc8f
mirpribili <mirpribili@ya.ru>
2023-07-19 14:59:44 +0300

    some fix

--- src/main/java/com/javacorner/admin/JavacornerAdminApplication.java
+++ src/main/java/com/javacorner/admin/JavacornerAdminApplication.java
@@ -1,24 +1,13 @@
 package com.javacorner.admin;
 
-
-import com.javacorner.admin.service.*;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class JavacornerAdminApplication {
 
-	/** Unit & Integration Testing Implementation Steps
-	 * To take:
-	 * 1. Use Spring Service Project.
-	 * 2. Creating Unit Tests for Service Layer using Junit-5 & Mockito-4.
-	 * 3. Configuring Test Containers for Integration Test.
-	 * 4. Testing Repositories using TestContainers & DataJpaTest.
-	 */
-
 	public static void main(String[] args) {
 		SpringApplication.run(JavacornerAdminApplication.class, args);
 	}
+
 }
